08671bf1633a66e7b0a71bc9a611d04ee3781bbb
Nayan <nayan92001@gmail.com>
2024-08-10 02:21:26 +0530

    Added more static content

--- src/components/download/Download.jsx
+++ src/components/download/Download.jsx
@@ -1,8 +1,32 @@
 import styles from "./Download.module.css";
+import Img from "../../assets/download.png";
+import Vector from "../../assets/Vector.png";
+import AppStore from "../../assets/AppStore.png";
+import PlayStore from "../../assets/PlayStore.png";
+import { Button, TextField } from "@mui/material";
+import { Google, Phone } from "@mui/icons-material";
 
 const Download = () => {
     return <>
-        <h2>Download</h2>
+        <div className={styles.container}>
+            <div className={styles.img}>
+                <img src={Img} alt="" />
+            </div>
+            <div className={styles.download}>
+                <div className={styles.title}>
+                    <h1>Download the </h1> <h1 className={styles.medify}> Medify </h1> <h1> App</h1>
+                </div>
+                <img className={styles.vector} src={Vector} alt="" />
+                <div>
+                    <p className={styles.cmt}>Get the link to download the app</p>
+                    <TextField InputProps={{ startAdornment: <Phone /> }} label="Phone Number" placeholder="Enter phone number" /> <Button sx={{ backgroundColor: "#2AA8FF", fontSize: "1em", padding: ".9em 2em" }} variant="contained">Send SMS</Button>
+                </div>
+                <div className={styles.store}>
+                    <img width="250px" src={PlayStore} alt="" />
+                    <img width="250px" src={AppStore} alt="" />
+                </div>
+            </div>
+        </div>
     </>;
 };
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,56 @@
+import { ArrowForwardIos, FacebookRounded, Pinterest, Twitter, YouTube } from "@mui/icons-material";
 import styles from "./Footer.module.css";
+
+import Logo from "../../assets/logo.png"
 
 const Footer = () => {
     return <>
-        <h2>Footer</h2>
+        <footer className={styles.container}>
+            <div className={styles.top}>
+                <div className={styles.social}>
+                    <div className={styles.logo}>
+                        <img src={Logo} alt="medify" />
+                    </div>
+                    <div className={styles.handles}>
+                        <FacebookRounded />
+                        <Twitter />
+                        <YouTube />
+                        <Pinterest />
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <ul>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> About Us</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Our Pricing</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Our Gallery</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Appointment</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Privacy Policy</li>
+                    </ul>
+                </div>
+                <div className={styles.content}>
+                    <ul>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Orthology</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Neurology</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Dental Care</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Opthalmology</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Cardiology</li>
+                    </ul>
+                </div>
+                <div className={styles.content}>
+                    <ul>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> About Us</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Our Pricing</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Our Gallery</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Appointment</li>
+                        <li><ArrowForwardIos sx={{ color: "white" }} /> Privacy Policy</li>
+                    </ul>
+                </div>
+            </div>
+            <hr className={styles.hr} />
+            <div className={styles.bottom}>
+                <p>Copyright ©2023 ___________________. All Rights Reserved</p>
+            </div>
+        </footer>
     </>;
 };
 
